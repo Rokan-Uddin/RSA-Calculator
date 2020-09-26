b13edb4af612ff3a764620bd5ca7dcdf1a000c44
Rokan Uddin <rokanu55@gmail.com>
2020-09-27 05:40:03 +0600

    get n,r,e,d from p,q

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import RSA from './components/RSA';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const arr=[];
+function gcd(a, b) {
+  if (b === 0) return a;
+  return gcd(b, a % b);
 }
 
+function populateEArray(r) {
+  for (var i = 2; i < r; ++i) {
+    if (gcd(i, r) === 1) arr.push(i);
+  }
+}
+function makeD(e,r) {
+  console.log(e,r);
+  for(var i=1;i<r;i++) {
+    if((Math.ceil((1+(i*r))/e))===((1+(i*r))/e)){
+      console.log(((1+(i*r))/e));
+      var ee=((1+(i*r))/e);
+      break;
+    }
+  }
+   return ee;
+}
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      p:undefined,
+      q:undefined,
+      e:undefined,
+      d:undefined,
+      n:undefined,
+      r:undefined,
+      array:undefined
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+     this.handleSubmit2 = this.handleSubmit2.bind(this);
+  }
+  handleSubmit2(event) {
+    const dd =makeD(event.target.e.value, this.state.r);
+    this.setState({
+      e:event.target.e.value,
+      d:dd
+    })
+    event.preventDefault();
+  }
+  handleSubmit(event) {
+      populateEArray((event.target.p.value-1)*(event.target.q.value-1));
+      this.setState({
+        p:event.target.p.value,
+        q:event.target.q.value,
+        n:event.target.p.value*event.target.q.value,
+        r:(event.target.p.value-1)*(event.target.q.value-1),
+        array:arr
+      })
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div className="container">
+      <h1 className="text-center mt-4">RSA Calculator </h1>
+      <RSA 
+      p={this.state.p}
+      q={this.state.q}
+      d={this.state.d}
+      n={this.state.n}
+      e={this.state.e}
+      r={this.state.r}
+      array={this.state.array}
+      handleSubmit={this.handleSubmit}
+      handleSubmit2={this.handleSubmit2}
+       />
+      </div>
+      );
+ }
+}
 export default App;
---
+++ src/components/RSA.js
@@ -0,0 +1,34 @@
+import React from 'react';
+function RSA(props) {
+	return(
+		<div className="container">
+			<div className="row  justify-content-center">
+			<form onSubmit={props.handleSubmit}>
+				<label>P:<input className="ml-2" name="p" /></label>
+				<label>Q:<input className="ml-2" name="q" /></label>
+				<input className="ml-2" type="submit" value="Submit" />
+			</form>
+			</div>
+
+			<div className=" row justify-content-center">
+			<p> N: {props.n}</p>
+			</div>
+			<div className=" row justify-content-center">
+			<p> r: {props.r}</p>
+			</div>
+
+			<div className="row">
+			Enter a value for e--
+			{props.array}
+			</div>
+			<div className="row">
+			<form onSubmit={props.handleSubmit2}>
+			<label> E: <input name="e" /> </label>
+			<input type="submit" value="Submit" />
+			</form>
+			</div>
+			<div className="row"> {props.d}  </div>
+		</div>
+		);
+}
+export default RSA;
